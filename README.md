48e281aaf0995c00a7980582e6c8c0f27696a3ab
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-17 13:05:09 -0700

    Organized files further

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ETL Pipeline
-## ETL Pipeline for Used Car Dealership
-*   Read CSV and JSON file types.
+## Aquired ETL Pipeline Skills
+*   Read CSV, JSON, and XML file types.
 *   Extract data from the above file types.
 *   Transform data.
-*   Save the transformed data in a ready-to-load format which data engineers can use to load into an RDBMS.
+*   Save the transformed data which can be used to load into an RDBMS.
--- Used Car Dealership/Main Pipeline.py
+++ Used Car Dealership/Main Pipeline.py
@@ -1,20 +1,25 @@
+# import modules required for etl
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
+# name files for easy access
 tmpfile    = "dealership_temp.tmp"
 logfile    = "dealership_logfile.txt"
 targetfile = "dealership_transformed_data.csv"
 
+# extract csv file function
 def extract_from_csv(file_to_process):
     dataframe = pd.read_csv(file_to_process)
     return dataframe
 
+# extract json file function
 def extract_from_json(file_to_process):
     dataframe = pd.read_json(file_to_process, lines=True)
     return dataframe
 
+# extract xml file function
 def extract_from_xml(file_to_process):
     dataframe = pd.DataFrame(columns=['car_model','year_of_manufacture','price', 'fuel'])
     tree = ET.parse(file_to_process)
@@ -27,6 +32,7 @@ def extract_from_xml(file_to_process):
         dataframe = dataframe.append({"car_model":car_model, "year_of_manufacture":year_of_manufacture, "price":price, "fuel":fuel}, ignore_index=True)
     return dataframe
 
+#  extract function
 def extract():
     extracted_data = pd.DataFrame(columns=['car_model','year_of_manufacture','price', 'fuel'])
     
@@ -41,13 +47,16 @@ def extract():
         
     return extracted_data
 
+# round to 2 decimal points (transform)
 def transform(data):
         data['price'] = round(data.price, 2)
         return data
 
+# load file
 def load(targetfile,data_to_load):
     data_to_load.to_csv(targetfile)  
 
+# log analytics
 def log(message):
     timestamp_format = '%H:%M:%S-%h-%d-%Y'
     now = datetime.now()
@@ -55,19 +64,23 @@ def log(message):
     with open("dealership_logfile.txt","a") as f:
         f.write(timestamp + ',' + message + '\n') 
 
+# log job started
 log("ETL Job Started")
 
+# log extract started, call extract function, and extract ended
 log("Extract phase Started")
 extracted_data = extract()
 log("Extract phase Ended")
 
+# log transform started, call transform function, and transform ended
 log("Transform phase Started")
 transformed_data = transform(extracted_data)
 log("Transform phase Ended")
 
+# log load started, call load function, and load ended
 log("Load phase Started")
 load(targetfile,transformed_data)
 log("Load phase Ended")
 
+# log etl job ended
 log("ETL Job Ended")
-
